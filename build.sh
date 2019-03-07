4de123f926bf0e995b02b187f28c50c8293b5596
Stephen <stephen@thoughtstem.com>
2019-03-07 13:36:52 -0800

    first

---
+++ build.sh
@@ -0,0 +1,14 @@
+raco pkg install https://github.com/thoughtstem/racket-chipmunk.git#dev
+raco pkg install https://github.com/thoughtstem/ratchet.git#dev
+
+raco pkg install https://github.com/thoughtstem/game-engine.git#dev
+raco pkg install https://github.com/thoughtstem/game-engine-rpg.git#dev
+raco pkg install https://github.com/thoughtstem/game-engine-demos-common.git#dev
+
+cd $TRAVIS_BUILD_DIR
+git clone -b dev https://github.com/thoughtstem/TS-Kata-Collections.git
+git clone -b dev https://github.com/thoughtstem/TS-Languages.git
+cd $TRAVIS_BUILD_DIR/TS-Kata-Collections/ts-kata-util && raco pkg install --deps search-auto
+cd $TRAVIS_BUILD_DIR/TS-Languages && git pull origin master && raco install-all-here
+cd $TRAVIS_BUILD_DIR/TS-Kata-Collections && git pull origin master && raco install-all-here
+
