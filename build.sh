33353fe340ff5f0d7deb7dd5e5a7fa7a2a31749b
Stephen <stephen@thoughtstem.com>
2019-03-08 15:14:38 -0800

    conditionalizing things based on branch name

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 echo "**************************"
 echo "INSTALLING RACKET CHIPMUNK"
 echo "**************************"
-raco pkg install --deps search-auto https://github.com/thoughtstem/racket-chipmunk.git#dev
+raco pkg install --deps search-auto https://github.com/thoughtstem/racket-chipmunk.git#$TRAVIS_BRANCH
 
 echo "**************************"
 echo "INSTALLING RATCHET"
 echo "**************************"
-raco pkg install --deps search-auto https://github.com/thoughtstem/ratchet.git#dev
+raco pkg install --deps search-auto https://github.com/thoughtstem/ratchet.git#$TRAVIS_BRANCH
 
 echo "**************************"
 echo "INSTALLING GAME-ENGINE-*"
 echo "**************************"
-raco pkg install --deps search-auto https://github.com/thoughtstem/game-engine.git#dev
-raco pkg install --deps search-auto https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common#dev
-raco pkg install --deps search-auto https://github.com/thoughtstem/game-engine-rpg.git#dev
+raco pkg install --deps search-auto https://github.com/thoughtstem/game-engine.git#$TRAVIS_BRANCH
+raco pkg install --deps search-auto https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common#$TRAVIS_BRANCH
+raco pkg install --deps search-auto https://github.com/thoughtstem/game-engine-rpg.git#$TRAVIS_BRANCH
 
 echo "**************************"
 echo "cd $TRAVIS_BUILD_DIR"
@@ -23,12 +23,12 @@ cd $TRAVIS_BUILD_DIR
 echo "**************************"
 echo "CLONING TS-Kata-Collections"
 echo "**************************"
-git clone -b dev https://github.com/thoughtstem/TS-Kata-Collections.git
+git clone -b $TRAVIS_BRANCH https://github.com/thoughtstem/TS-Kata-Collections.git
 
 echo "**************************"
 echo "CLONING TS-Languages"
 echo "**************************"
-git clone -b dev https://github.com/thoughtstem/TS-Languages.git
+git clone -b $TRAVIS_BRANCH https://github.com/thoughtstem/TS-Languages.git
 
 echo "**************************"
 echo "INSTALLING ts-kata-util"
--- host-docs.sh
+++ host-docs.sh
@@ -1,21 +1,26 @@
 #Exit if commands fail.  Don't want to push weird stuff to docs.
 set -e
 
-echo "*****************************"
-echo "Cloning katas.thoughtstem.com"
-echo "*****************************"
+if ["$TRAVIS_BRANCH" = "master" ]
+then
+  echo "*****************************"
+  echo "Cloning katas.thoughtstem.com"
+  echo "*****************************"
 
-git clone https://$GH_TOKEN@github.com/thoughtstem/katas.thoughtstem.com.git
+  git clone https://$GH_TOKEN@github.com/thoughtstem/katas.thoughtstem.com.git
 
-echo "*****************************"
-echo "Moving over doc files"
-echo "*****************************"
-rm -rf katas.thoughtstem.com/* && find $TRAVIS_BUILD_DIR/TS-Kata-Collections/* -name "doc" | xargs cp -r --parents -t ./katas.thoughtstem.com/ && mv ./katas.thoughtstem.com/TS-Kata-Collections/* ./katas.thoughtstem.com/ && rm -r ./katas.thoughtstem.com/TS-Kata-Collections && $TRAVIS_BUILD_DIR/TS-Kata-Collections/index.html ./katas.thoughtstem.com
+  echo "*****************************"
+  echo "Moving over doc files"
+  echo "*****************************"
+  rm -rf katas.thoughtstem.com/* && find $TRAVIS_BUILD_DIR/TS-Kata-Collections/* -name "doc" | xargs cp -r --parents -t ./katas.thoughtstem.com/ && mv ./katas.thoughtstem.com/TS-Kata-Collections/* ./katas.thoughtstem.com/ && rm -r ./katas.thoughtstem.com/TS-Kata-Collections && $TRAVIS_BUILD_DIR/TS-Kata-Collections/index.html ./katas.thoughtstem.com
 
-echo "*****************************"
-echo "PUSHING it back to github"
-echo "*****************************"
-cd katas.thoughtstem.com 
-git add .
-git commit -m "Travis Build: $TRAVIS_BUILD_NUMBER"
-git push origin master
+  echo "*****************************"
+  echo "PUSHING it back to github"
+  echo "*****************************"
+  cd katas.thoughtstem.com 
+  git add .
+  git commit -m "Travis Build: $TRAVIS_BUILD_NUMBER"
+  git push origin master
+else
+  echo "NOT MASTER, SO NOT PUSHING DOCS"
+fi
