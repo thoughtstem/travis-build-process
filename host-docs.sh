47ecbaff13dc7afb2f427bd0f67a8226a9b210cd
Stephen <stephen@thoughtstem.com>
2019-03-08 15:45:49 -0800

    fixing host docs script

--- host-docs.sh
+++ host-docs.sh
@@ -1,7 +1,7 @@
 #Exit if commands fail.  Don't want to push weird stuff to docs.
 set -e
 
-if ["$TRAVIS_BRANCH" = "master" ]
+if [ "$TRAVIS_BRANCH" = "master" ]
 then
   echo "*****************************"
   echo "Cloning katas.thoughtstem.com"
@@ -12,7 +12,7 @@ then
   echo "*****************************"
   echo "Moving over doc files"
   echo "*****************************"
-  rm -rf katas.thoughtstem.com/* && find $TRAVIS_BUILD_DIR/TS-Kata-Collections/* -name "doc" | xargs cp -r --parents -t ./katas.thoughtstem.com/ && mv ./katas.thoughtstem.com/TS-Kata-Collections/* ./katas.thoughtstem.com/ && rm -r ./katas.thoughtstem.com/TS-Kata-Collections && $TRAVIS_BUILD_DIR/TS-Kata-Collections/index.html ./katas.thoughtstem.com
+  rm -rf katas.thoughtstem.com/* && find $TRAVIS_BUILD_DIR/TS-Kata-Collections/* -name "doc" | xargs cp -r --parents -t ./katas.thoughtstem.com/ && mv ./katas.thoughtstem.com/TS-Kata-Collections/* ./katas.thoughtstem.com/ && rm -r ./katas.thoughtstem.com/TS-Kata-Collections && cp $TRAVIS_BUILD_DIR/TS-Kata-Collections/index.html ./katas.thoughtstem.com/ && cp $TRAVIS_BUILD_DIR/TS-Kata-Collections/unlisted.html ./katas.thoughtstem.com/
 
   echo "*****************************"
   echo "PUSHING it back to github"
