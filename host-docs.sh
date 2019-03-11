ebc7f07e8241ad393a32a8e04cba46a90b08c267
Stephen <stephen@thoughtstem.com>
2019-03-11 13:20:36 -0700

    fixing doc hosting script

--- host-docs.sh
+++ host-docs.sh
@@ -12,12 +12,13 @@ then
   echo "*****************************"
   echo "Moving over doc files"
   echo "*****************************"
-  rm -rf katas.thoughtstem.com/* && find $TRAVIS_BUILD_DIR/TS-Kata-Collections/* -name "doc" | xargs cp -r --parents -t ./katas.thoughtstem.com/ && mv ./katas.thoughtstem.com/TS-Kata-Collections/* ./katas.thoughtstem.com/ && rm -r ./katas.thoughtstem.com/TS-Kata-Collections && cp $TRAVIS_BUILD_DIR/TS-Kata-Collections/index.html ./katas.thoughtstem.com/ && cp $TRAVIS_BUILD_DIR/TS-Kata-Collections/unlisted.html ./katas.thoughtstem.com/
+  rm -rf katas.thoughtstem.com/* && find $TRAVIS_BUILD_DIR/TS-Kata-Collections/* -name "doc" | xargs cp -r --parents -t ./katas.thoughtstem.com/ && mv ./katas.thoughtstem.com/$TRAVIS_BUILD_DIR/TS-Kata-Collections/* ./katas.thoughtstem.com/ && rm -r ./katas.thoughtstem.com/$TRAVIS_BUILD_DIR/TS-Kata-Collections && cp $TRAVIS_BUILD_DIR/TS-Kata-Collections/index.html ./katas.thoughtstem.com/ && cp $TRAVIS_BUILD_DIR/TS-Kata-Collections/unlisted.html ./katas.thoughtstem.com/
 
   echo "*****************************"
   echo "PUSHING it back to github"
   echo "*****************************"
   cd katas.thoughtstem.com 
+  echo "katas.thoughtstem.com" > CNAME
   git add .
   git commit -m "Travis Build: $TRAVIS_BUILD_NUMBER"
   git push origin master
