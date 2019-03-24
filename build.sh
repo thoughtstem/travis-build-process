393e9c15f9ec6182bc8768524071eb269cae3e4f
Stephen <stephen@thoughtstem.com>
2019-03-23 17:29:41 -0700

    attempted speed up

--- build.sh
+++ build.sh
@@ -4,22 +4,27 @@ set -e
 echo "**************************"
 echo "INSTALLING RACKET CHIPMUNK"
 echo "**************************"
-raco pkg install --deps search-auto https://github.com/thoughtstem/racket-chipmunk.git#$TRAVIS_BRANCH
+raco pkg install --deps search-auto --no-setup https://github.com/thoughtstem/racket-chipmunk.git#$TRAVIS_BRANCH
+raco setup --no-docs --fail-fast racket-chipmunk 
 
 echo "**************************"
 echo "INSTALLING RATCHET"
 echo "**************************"
-raco pkg install --deps search-auto https://github.com/thoughtstem/ratchet.git#$TRAVIS_BRANCH
+raco pkg install --deps search-auto --no-setup https://github.com/thoughtstem/ratchet.git#$TRAVIS_BRANCH
+raco setup --no-docs --fail-fast ratchet
+
 
 echo "**************************"
 echo "INSTALLING GAME-ENGINE-*"
 echo "**************************"
 sudo apt-get update
 sudo apt-get install libportaudio2
-sudo modprobe snd-dummy
-raco pkg install --deps search-auto https://github.com/thoughtstem/game-engine.git#$TRAVIS_BRANCH
-raco pkg install --deps search-auto https://github.com/thoughtstem/game-engine-rpg.git#$TRAVIS_BRANCH
-raco pkg install --deps search-auto https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common#$TRAVIS_BRANCH
+raco pkg install --deps search-auto --no-setup https://github.com/thoughtstem/game-engine.git#$TRAVIS_BRANCH
+raco setup --no-docs --fail-fast game-engine
+raco pkg install --deps search-auto --no-setup https://github.com/thoughtstem/game-engine-rpg.git#$TRAVIS_BRANCH
+raco setup --no-docs --fail-fast game-engine-rpg
+raco pkg install --deps search-auto --no-setup https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common#$TRAVIS_BRANCH
+raco setup --no-docs --fail-fast game-engine-demos-common
 
 echo "**************************"
 echo "cd $TRAVIS_BUILD_DIR"
@@ -39,7 +44,8 @@ git clone -b $TRAVIS_BRANCH https://github.com/thoughtstem/TS-Languages.git
 echo "**************************"
 echo "INSTALLING ts-kata-util"
 echo "**************************"
-cd $TRAVIS_BUILD_DIR/TS-Kata-Collections/ts-kata-util && raco pkg install --deps search-auto
+cd $TRAVIS_BUILD_DIR/TS-Kata-Collections/ts-kata-util && raco pkg install --deps search-auto --no-setup   
+raco setup --no-docs --fail-fast ts-kata-util
 
 echo "**************************"
 echo "INSTALLING TS-Languages"
