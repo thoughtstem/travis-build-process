b1aa8dc52f00606a055ea5fe9eff8b396745fe12
srfoster <stephen@thoughtstem.com>
2019-09-20 14:50:52 -0700

    fix

--- VR/build.sh
+++ VR/build.sh
@@ -2,13 +2,7 @@
 set -e
 
 echo "**************************"
-echo "INSTALLING ts-kata-util"
-echo "**************************"
-raco pkg install --deps search-auto --no-setup https://github.com/thoughtstem/TS-Kata-Collections.git#$TRAVIS_BRANCH
-raco setup --no-docs --fail-fast vr-engine
-
-echo "**************************"
-echo "CLONING ts-kata-util"
+echo "CLONING and INSTALLING ts-kata-util"
 echo "**************************"
 git clone -b $TRAVIS_BRANCH https://github.com/thoughtstem/TS-Kata-Collections.git#$TRAVIS_BRANCH
 cd $TRAVIS_BUILD_DIR/TS-Kata-Collections/ts-kata-util && raco pkg install --deps search-auto --no-setup
